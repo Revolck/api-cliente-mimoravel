9e487d125db1bddb3b3724a5ad66b1d6a0f76ad4
Revolck <165503105+Revolck@users.noreply.github.com>
2024-08-13 22:49:52 -0300

    alteração da rota

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-router.post('/users', userController.addUser);
+router.post('/register', userController.addUser);
 router.post('/login', userController.loginUser);
 
 module.exports = router;
