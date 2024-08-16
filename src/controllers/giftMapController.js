a0a3ad69369cf76de1ae3451ba407b504453ea23
Revolck <165503105+Revolck@users.noreply.github.com>
2024-08-16 11:46:35 -0300

    criando conexao com tabela dashboard

--- src/controllers/giftMapController.js
+++ src/controllers/giftMapController.js
@@ -1,8 +1,10 @@
 const giftMapService = require('../services/giftMapService');
 
 exports.getGiftMap = async (req, res, next) => {
+    const { search } = req.query;
+
     try {
-        const giftMaps = await giftMapService.getGiftMap();
+        const giftMaps = await giftMapService.getGiftMap(search);
         res.json(giftMaps);
     } catch (err) {
         next(err);
@@ -17,3 +19,12 @@ exports.addGiftMap = async (req, res, next) => {
         next(err);
     }
 };
+
+exports.getAllGiftMaps = async (req, res) => {
+    try {
+      const giftMaps = await giftMapService.getAllGiftMaps();
+      res.status(200).json(giftMaps);
+    } catch (error) {
+      res.status(500).json({ message: 'Erro ao buscar os dados do gift_map' });
+    }
+  };
--- src/services/giftMapService.js
+++ src/services/giftMapService.js
@@ -1,13 +1,38 @@
 const connection = require('../config/db');
 
-const getGiftMap = async () => {
+const getGiftMap = async (search) => {
+    let query = `
+        SELECT gm.id, r.nome AS name, r.email, r.telefone AS phone, r.cidade AS city, gm.data_criacao AS sendDate, gm.status_contato AS status
+        FROM gift_map gm
+        JOIN respondents r ON gm.respondent_id = r.id
+    `;
+
+    const params = [];
+
+    if (search) {
+        query += `
+            WHERE r.nome LIKE ? OR r.email LIKE ? OR r.telefone LIKE ?
+        `;
+        const searchValue = `%${search}%`; // Usa o wildcard '%' para pesquisa parcial
+        params.push(searchValue, searchValue, searchValue);
+    }
+
     try {
-        const [rows] = await connection.query('SELECT * FROM gift_map');
+        const [rows] = await connection.query(query, params);
         return rows;
     } catch (err) {
         throw new Error('Erro ao obter respostas do quizz: ' + err.message);
     }
 };
+
+const getAllGiftMaps = async () => {
+    const [rows] = await db.query(`
+      SELECT gm.id, r.nome AS name, r.email, r.telefone AS phone, r.cidade AS city, gm.data_criacao AS sendDate, gm.status_contato AS status
+      FROM gift_map gm
+      JOIN respondents r ON gm.respondent_id = r.id
+    `);
+    return rows;
+  };
 
 const addGiftMap = async (giftMapData) => {
     const { respondent_id, ocasiao, relacionamento, paixoes_hobbies, estilo, melhores_momentos, valores, desejos, avesso_a, status_contato } = giftMapData;
@@ -24,5 +49,6 @@ const addGiftMap = async (giftMapData) => {
 
 module.exports = {
     getGiftMap,
-    addGiftMap
+    addGiftMap,
+    getAllGiftMaps,
 };
