c0b60f4252250a9c3be69b1db0bc25738562cce6
Revolck <165503105+Revolck@users.noreply.github.com>
2024-08-17 11:42:22 -0300

    ajustando bug de conexao timeout com getbyid

--- src/config/db.js
+++ src/config/db.js
@@ -6,7 +6,19 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    connectionLimit: 10
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
+
+// Verifica a conexão com o banco de dados
+pool.getConnection()
+    .then(connection => {
+        console.log('Conexão ao banco de dados bem-sucedida!');
+        connection.release(); // Libera a conexão de volta para o pool
+    })
+    .catch(err => {
+        console.error('Erro ao conectar ao banco de dados:', err.message);
+    });
 
 module.exports = pool;
