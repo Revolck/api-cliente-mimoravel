f004966170c6637bc77b7e1367f1ccf594a1537a
Revolck <165503105+Revolck@users.noreply.github.com>
2024-08-21 12:25:36 -0300

    data

--- src/services/chartGiftMapService.js
+++ src/services/chartGiftMapService.js
@@ -1,9 +1,11 @@
 const connection = require('../config/db');
+const { format, addDays, startOfWeek } = require('date-fns');
+const { ptBR } = require('date-fns/locale');
 
 // Função para obter novos leads por semana
 const getNewLeadsByWeek = async () => {
   const [rows] = await connection.query(`
-    SELECT DATE_FORMAT(data_criacao, '%W') AS day_name, DATE_FORMAT(data_criacao, '%d/%m') AS date, COUNT(*) AS 'Novo Lead'
+    SELECT DATE_FORMAT(data_criacao, '%W') AS day_name, DATE_FORMAT(data_criacao, '%Y-%m-%d') AS date, COUNT(*) AS 'Novo Lead'
     FROM gift_map
     WHERE status_contato = 'Novo Lead'
     AND data_criacao >= CURDATE() - INTERVAL WEEKDAY(CURDATE()) DAY
@@ -11,13 +13,15 @@ const getNewLeadsByWeek = async () => {
   `);
 
   const weekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+  const today = new Date();
+  const startDate = startOfWeek(today, { locale: ptBR, weekStartsOn: 0 });
 
   const completeWeekData = weekDays.map((day, index) => {
     const found = rows.find(row => row.day_name === day);
-    const date = found ? found.date : format(new Date(Date.now() - (WEEKDAY(new Date()) - index) * 86400000), 'dd/MM');
+    const date = found ? found.date : format(addDays(startDate, index), 'dd/MM');
     return {
       day: `${day} - ${date}`,
-      'Novo Lead': found ? found['Novo Lead'] : 0
+      'Novo Lead': found ? found['Novo Lead'] : 0,
     };
   });
 
