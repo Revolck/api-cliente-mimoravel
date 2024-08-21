181c62ef6262f0ba8810af805274cdf2e7eaa8c1
Revolck <165503105+Revolck@users.noreply.github.com>
2024-08-20 22:57:52 -0300

    ajustando rota

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(morgan('combined'));
 // Rotas da API
 app.use('/api/users', require('./src/routes/userRoutes'));
 app.use('/api/giftmaps', require('./src/routes/giftMapRoutes'));
+app.use('/api/chart', require('./src/routes/chartGiftMapRoutes'));
 // app.use('/api/auth', require('./src/routes/authRoutes'));
 
 // Middleware de tratamento de erros
