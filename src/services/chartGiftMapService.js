6cce5ca309b91d8cf44dda1394a265c31fd4a338
Revolck <165503105+Revolck@users.noreply.github.com>
2024-08-20 23:40:17 -0300

    traducao

--- src/services/chartGiftMapService.js
+++ src/services/chartGiftMapService.js
@@ -15,11 +15,26 @@ const getNewLeadsByWeek = async () => {
 // Função para obter novos leads por mês
 const getNewLeadsByMonth = async () => {
   const [rows] = await connection.query(`
-    SELECT DATE_FORMAT(data_criacao, '%M/%Y') AS month, COUNT(*) AS 'Novo Lead'
+    SELECT
+      CASE
+        WHEN MONTH(data_criacao) = 1 THEN 'Janeiro'
+        WHEN MONTH(data_criacao) = 2 THEN 'Fevereiro'
+        WHEN MONTH(data_criacao) = 3 THEN 'Março'
+        WHEN MONTH(data_criacao) = 4 THEN 'Abril'
+        WHEN MONTH(data_criacao) = 5 THEN 'Maio'
+        WHEN MONTH(data_criacao) = 6 THEN 'Junho'
+        WHEN MONTH(data_criacao) = 7 THEN 'Julho'
+        WHEN MONTH(data_criacao) = 8 THEN 'Agosto'
+        WHEN MONTH(data_criacao) = 9 THEN 'Setembro'
+        WHEN MONTH(data_criacao) = 10 THEN 'Outubro'
+        WHEN MONTH(data_criacao) = 11 THEN 'Novembro'
+        WHEN MONTH(data_criacao) = 12 THEN 'Dezembro'
+      END AS month,
+      COUNT(*) AS 'Novo Lead'
     FROM gift_map
     WHERE status_contato = 'Novo Lead'
     AND YEAR(data_criacao) = YEAR(CURDATE())
-    GROUP BY DATE_FORMAT(data_criacao, '%Y-%m')
+    GROUP BY MONTH(data_criacao), YEAR(data_criacao)
   `);
   return rows;
 };
